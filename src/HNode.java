bc9a28ba252f6a9cfc69c0ae83cb14095d6f9ed0
xTriixrx <vnigro5743@gmail.com>
2022-12-24 16:04:10 -0500

    initial commit

---
+++ src/HNode.java
@@ -0,0 +1,24 @@
+
+public class HNode{
+	
+	int freq;
+	char ch;
+	HNode left, right, parent;
+	String code = "";
+	
+	public HNode()
+	{
+		this.freq = 0;
+		this.ch = ' ';
+	}
+	
+	public HNode(char letter, int freq){
+		this.ch = letter;
+		this.freq = freq;
+	
+	}
+	
+	
+	
+
+}
